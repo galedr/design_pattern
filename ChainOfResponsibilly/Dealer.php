329a29b263326f88b9e6e88c5ffe48faeb320488
galedr <galedr1985@gmail.com>
2017-12-23 00:56:34 +0800

    fix : 責任練文字敘述

--- ChainOfResponsibilly/Dealer.php
+++ ChainOfResponsibilly/Dealer.php
@@ -7,7 +7,7 @@ class FriendZone extends Handler
     public function response(Request $request)
     {
         if (in_array($request->doRequest(), $this->capability)) {
-            echo '好喔，我們是好朋友，我們可以' . $request->doRequest();
+            echo '好朋友的話' . $request->doRequest() . '還可以，你好臭，離我遠一點';
         } else {
             if ($this->next != null) {
                 $this->next->response($request);
@@ -18,12 +18,12 @@ class FriendZone extends Handler
 
 class AmbiguousZone extends Handler
 {
-    protected $capability = array('吃飯', '牽手');
+    protected $capability = array('牽手', '擁抱');
 
     public function response(Request $request)
     {
         if (in_array($request->doRequest(), $this->capability)) {
-            echo '好喔，我們之間有點曖昧，我們可以' . $request->doRequest();
+            echo '有曖昧的話' . $request->doRequest() . '沒關係，但是你好臭，離我遠一點';
         } else {
             if ($this->next != null) {
                 $this->next->response($request);
@@ -34,12 +34,12 @@ class AmbiguousZone extends Handler
 
 class LoverZone extends Handler
 {
-    protected $capability = array('吃飯', '牽手', '親吻');
+    protected $capability = array('接吻');
 
     public function response(Request $request)
     {
         if (in_array($request->doRequest(), $this->capability)) {
-            echo '好喔，我們是情侶，我們可以' . $request->doRequest() . '還可以幹別的羞羞的事';
+            echo '情侶的話' . $request->doRequest() . '當然可以囉，你好臭，離我遠一點';
         } else {
             echo '你想幹嘛，我不知道' . $request->doRequest() . '是啥，你再不走我要報警囉';
         }
